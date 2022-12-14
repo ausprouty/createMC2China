09f74f2799554d302e419870d001300a4dc0c6d6
ausprouty <bob.prouty@powertochange.org.au>
2022-12-14 18:35:58 +1100

    Added Hope

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 // see    https://stackoverflow.com/questions/46590760/vue-js-2-multiple-routing-files
+import routesEngHope from "./routesEngHope.js";
 import routesEngMultiply1 from "./routesEngMultiply1.js";
 import routesEngMultiply2 from "./routesEngMultiply2.js";
 import routesEngMultiply3 from "./routesEngMultiply3.js";
+import routesEngProphet from "./routesEngProphet.js";
 import routesEngTc from "./routesEngTc.js";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
 
+    ...routesEngHope,
     ...routesEngMultiply1,
     ...routesEngMultiply2,
     ...routesEngMultiply3,
+    ...routesEngProphet,
      ...routesEngTc,
 
      {
---
+++ src/views/M2/eng/seek.vue
@@ -0,0 +1,49 @@
+<script>
+import Footer from '@/components/FooterGlobal.vue'
+
+export default {
+  components: {
+    Footer
+  },
+  methods:{
+    vuePush(id){
+      this.$router.push({
+        name: id,
+      })
+    },
+  },
+}
+</script>
+<template>
+  <!-- sdcard template from mc2 -->
+  
+  <div class="page_content" dir="ltr">
+    <div>
+      
+    </div>
+    <div>
+      <!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('eng-hope-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/standard/Stories-of-Hope.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('eng-prophet-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/standard/Stories-of-the-Prophets.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+
+    </div>
+    <Footer/>
+</div>
+<!-- end default library -->
+</template>
+<!--- Created by publishLibrary-->
