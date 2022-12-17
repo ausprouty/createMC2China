fb8ff4879da2f889f0566592bc4e40bfe7a4b3f3
ausprouty <bob.prouty@powertochange.org.au>
2022-12-17 17:29:49 +1100

    changed reveal Media

--- src/assets/javascript/revealMedia.js
+++ src/assets/javascript/revealMedia.js
@@ -20,24 +20,10 @@ export async function useRevealMedia(series_path) {
   });
 
   function setupMediaListeners(){
-    let class_name = 'external-movie'
-    let template =`
-      <video id="video[i]" width="100%" controls="controls" preload="metadata" autoplay="autoplay"
-          webkit-playsinline="webkit-playsinline" class="videoPlayer">
-          <source src="[url]" type="video/mp4">
-      `
-    setupMediaListener( class_name, template)
-    class_name = 'external-audio'
-    template =`
-    <audio id="audio[i]" width="100%" controls >
-          <source src="[url]" type="audio/mpeg">
-          Your browser does not support the audio element.
-    </audio>
-    `
-    setupMediaListener( class_name, template)
-
+    setupMediaListener( 'external-movie')
+    setupMediaListener( 'external-audio')
   }
-  function setupMediaListener(class_name, template){
+  function setupMediaListener(class_name){
     var coll = document.getElementsByClassName(class_name)
     var i
     for (i = 0; i < coll.length; i++) {
@@ -48,10 +34,18 @@ export async function useRevealMedia(series_path) {
         var filePath= localStorage.getItem('sd_filepath') + '/'+ id
         var readable = await canRead(filePath)
         console.log(readable)
-         var content = this.nextElementSibling
+        var content = this.nextElementSibling
         if (readable){
+          console.log('this file is readable')
           var url = localStorage.getItem('sd_url') + '/'+ id
           if (this.classList.contains('active') ){
+            let template = ''
+            if (id.includes("/audio/")){
+                template = audioTemplate();
+            }
+            else{
+                template = videoTemplate();
+            }
             let temp = template.replace('[url]', url)
             let media = temp.replace('[i]', i)
             content.innerHTML = media
@@ -74,16 +68,36 @@ export async function useRevealMedia(series_path) {
       })
     }
   }
+  function audioTemplate(){
+    let template =`
+    <audio id="audio[i]" width="100%" controls >
+          <source src="[url]" type="audio/mpeg">
+          Your browser does not support the audio element.
+    </audio>
+    `
+    return template
+
+  }
+  function videoTemplate(){
+    let template =`
+      <video id="video[i]" width="100%" controls="controls" preload="metadata" autoplay="autoplay"
+          webkit-playsinline="webkit-playsinline" class="videoPlayer">
+          <source src="[url]" type="video/mp4">
+      `
+      return template
+  }
   async function canRead(file_name){
     await Filesystem.stat({
       path: file_name
     }).then(async (response) =>{
     console.log ('can read')
     console.log (JSON.stringify(response) )
+    console.log ('returning true')
     return true
     }).catch(error=>{
       console.log("error canRead")
       console.log(error);
+      console.log ('returning false')
       return false
     });
   }
